cc5ea04a38ef701ae0644e1f265b1697ccde8fd6
Aaron Brooks <pleaseshutupaaron@gmail.com>
2023-06-28 15:43:36 -0400

    loading and desktop with proper apps

---
+++ Desk Duty/Assets/DreamOS - Complete OS UI/Scripts/Apps/Reminder/ReminderManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using TMPro;
+
+namespace Michsky.DreamOS
+{
+    public class ReminderManager : MonoBehaviour
+    {
+        // Resource
+
+        // Multi Instance Support
+        void Awake()
+        {
+
+        }
+
+    }
+}
